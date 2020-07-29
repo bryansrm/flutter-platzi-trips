65e54ef9b7c3b319244885261bd1cc6f339e3920
Bryan Rodriguez <bsteven1993@gmail.com>
2020-07-28 22:28:58 -0500

    boton login with google

--- lib/User/ui/screens/sign_in.dart
+++ lib/User/ui/screens/sign_in.dart
@@ -1,7 +1,7 @@
-import 'package:design_app_pz/widgets/gradient_back.dart';
 import 'package:flutter/material.dart';
 
-
+import 'package:design_app_pz/widgets/gradient_back.dart';
+import 'package:design_app_pz/widgets/button_green.dart';
 
 class SignInPage extends StatefulWidget {
   @override
@@ -20,14 +20,21 @@ class _SignInPageState extends State<SignInPage> {
         children: <Widget>[
           GradientBack("", null),
           Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Welcome \n This is your travel app',
+              Text(' Welcome \n This is your travel app',
                 style: TextStyle(
                   fontSize: 38.0,
                   fontFamily: "Lato",
                   color: Colors.white,
                   fontWeight: FontWeight.bold
                 ),  
+              ),
+              ButtonGreen(
+                text: 'Login with Google',
+                width: 300.0,
+                height: 50.0,
+                onPressed: (){}
               )
             ],
           )
